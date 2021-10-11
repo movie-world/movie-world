9a867c27bf532cd0cb59c244ca8c3e09ca80735a
antnf3 <antnf3@gmail.com>
2021-10-12 06:59:58 +0900

    spinner 추가

--- src/components/loading.tsx
+++ src/components/loading.tsx
@@ -1,8 +1,10 @@
 import React from "react";
+import { ScaleLoader } from "react-spinners";
 
 export const Loading = () => {
   return (
-    <div className="bg-gray-900 w-screen h-screen flex items-center justify-center text-center text-white">
+    <div className="bg-gray-900 w-screen h-screen flex flex-col items-center justify-center text-center text-white">
+      <ScaleLoader height="80px" width="32px" color="#6b5ce7" radius="8px" />
       <p>loading...</p>
     </div>
   );
