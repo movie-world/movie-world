92cd13427b2b295f9e5ac9c4679081e8f2c78250
antnf3 <antnf3@gmail.com>
2021-11-07 14:42:45 +0900

    update

--- src/apollo.ts
+++ src/apollo.ts
@@ -4,7 +4,7 @@ const isLogedIn = localStorage.getItem("isLogedIn");
 export const isLoggedInVar = makeVar(Boolean(isLogedIn));
 
 export const client = new ApolloClient({
-  uri: "https://movie-world-server.herokuapp.com/",
+  uri: "https://movie-world-server.herokuapp.com/graphql",
   cache: new InMemoryCache({
     typePolicies: {
       Query: {
